6d4fc252c95f8c83bdd5fb0c1648f7135904f552
atef-reda <89020766+atef-reda@users.noreply.github.com>
2024-09-12 11:52:41 +0300

    lab4

---
+++ Cmake/Labs/Lab4/build/CMakeFiles/hellobinary.dir/src/calc.cpp.obj.d
@@ -0,0 +1,2 @@
+CMakeFiles/hellobinary.dir/src/calc.cpp.obj: \
+ D:\courses\embedded\ course\Embedded\ system\Cmake\Labs\Lab4\src\calc.cpp
---
+++ Cmake/Labs/Lab4/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+// #include "include/calc.hpp"
+#include "lib/wifi.hpp"
+int main()
+{
+    wifi_init();
+    return 0;
+}
